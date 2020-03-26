e3b89996ade49b9c75f2c527c23a2420acde2065
Edward Bustos <esbgo97@gmail.com>
2020-03-26 10:53:22 -0500

    addign masp feature

--- src/ui/screens/DetailedCountry.js
+++ src/ui/screens/DetailedCountry.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { createRef, useState } from 'react'
 import { View, Text, Button } from "react-native"
+import MapView from 'react-native-maps'
 
 import GlobalStyles from '../../utils/GlobalStyles'
 import { SvgUri } from 'react-native-svg'
@@ -19,6 +20,17 @@ const DetailedCountry = ({ route, navigation }) => {
         </View>)
     }
     let country = route.params.country
+
+    const [region, setRegion] = useState({
+        latitude: country.latlng[0],
+        longitude: country.latlng[1],
+        latitudeDelta: 1,
+        longitudeDelta: 1,
+    })
+
+    const mapRef = createRef();
+
+
     return <View style={GlobalStyles.body}>
         <SvgUri
             width={100}
@@ -28,13 +40,22 @@ const DetailedCountry = ({ route, navigation }) => {
         <Text style={GlobalStyles.title}>{country.name}</Text>
 
         <Text>Region: {country.region}</Text>
-        <Box height={20} />
+        <Text>Location: {country.latlng[0]} | {country.latlng[1]}</Text>
         <View>
-            <Text >Languajes:</Text>
+            <Text >Languajes: </Text>
             {country.languages.map((lang, i) => <Text style={GlobalStyles.langs} key={i} >- {lang.name}: {lang.nativeName}</Text>)}
         </View>
         <Box height={20} />
-        <Button title="Back" onPress={() => navigation.navigate("Countries")} />
+        <MapView
+            ref={mapRef}
+            region={region}
+            style={{ height: 300, width: "100%" }}
+            initialRegion={region}
+            onRegionChange={(region) => setRegion(region)}
+        />
+        <Box height={10} />
+        <Text>Position: ({region.latitude})({region.longitude})</Text>
+        <Button title="Back" onPress={() => navigation.goBack()} />
     </View>
 }
 
